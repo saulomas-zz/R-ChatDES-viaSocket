ac0e1bc6f9687c77aaa55fe08a7ce09539629659
Saulo <saulomas@gmail.com>
2019-09-14 22:18:35 -0300

    Construção das Funções

--- Chat/Util.r
+++ Chat/Util.r
@@ -1,7 +1,29 @@
+#install.packages('chunk2', repos='http://cran.us.r-project.org')
+
 #rawToChar(packBits(rawToBits(charToRaw("64bitKey")), type = c("raw", "integer")))
 
-permIni = c(
-    58, 50, 42, 34, 26, 18, 10,  2,
+pc1 = c(
+   57, 49, 41, 33, 25, 17,  9,
+    1, 58, 50, 42, 34, 26, 18,
+   10,  2, 59, 51, 43, 35, 27,
+   19, 11,  3, 60, 52, 44, 36,
+   63, 55, 47, 39, 31, 23, 15,
+    7, 62, 54, 46, 38, 30, 22,
+   14,  6, 61, 53, 45, 37, 29,
+   21, 13,  5, 28, 20, 12,  4)
+pc2 = c(
+   14, 17, 11, 24,  1,  5,
+    3, 28, 15,  6, 21, 10,
+   23, 19, 12,  4, 26,  8,
+   16,  7, 27, 20, 13,  2,
+   41, 52, 31, 37, 47, 55,
+   30, 40, 51, 45, 33, 48,
+   44, 49, 39, 56, 34, 53,
+   46, 42, 50, 36, 29, 32)
+
+shifts = c(1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1)
+
+permIni = c(58, 50, 42, 34, 26, 18, 10,  2,
     60, 52, 44, 36, 28, 20, 12,  4,
     62, 54, 46, 38, 30, 22, 14,  6,
     64, 56, 48, 40, 32, 24, 16,  8,
@@ -10,8 +32,7 @@ permIni = c(
     61, 53, 45, 37, 29, 21, 13,  5,
     63, 55, 47, 39, 31, 23, 15,  7)
 
-permFim = c(
-    40,  8, 48, 16, 56, 24, 64, 32,
+permFim = c(40,  8, 48, 16, 56, 24, 64, 32,
     39,  7, 47, 15, 55, 23, 63, 31,
     38,  6, 46, 14, 54, 22, 62, 30,
     37,  5, 45, 13, 53, 21, 61, 29,
@@ -21,12 +42,15 @@ permFim = c(
     33,  1, 41,  9, 49, 17, 57, 25)
 
 dBox = c(
-    32,  1,  2,  3,  4,  5,  4,  5,
-     6,  7,  8,  9,  8,  9, 10, 11,
-    12, 13, 12, 13, 14, 15, 16, 17,
-    16, 17, 18, 19, 20, 21, 20, 21,
-    22, 23, 24, 25, 24, 25, 26, 27,
-    28, 29, 28, 29, 30, 31, 32,  1)
+    32,  1,  2,  3,  4,  5,
+     4,  5,  6,  7,  8,  9, 
+     8,  9, 10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17,
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32,  1
+)
 
 sBox1l0 = c(14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7)
 sBox1l1 = c( 0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8)
@@ -108,6 +132,10 @@ rm(sBox1, sBox2, sBox3, sBox4, sBox5, sBox6, sBox7, sBox8)
 # rm -> remove objetos
 # sBox[[1]][[1]][[1]] -> pega o primeiro elemento da primeira lista que está dentro da primeira lista
 
+#Permutation
+#ExpansionPermutation
+#PermutedChoice
+
 pBox = c(
     16,  7, 20, 21, 29, 12, 28, 17,
      1, 15, 23, 26,  5, 18, 31, 10,
@@ -128,5 +156,106 @@ permute = function(data, initial) {
     return(dataTemp)
 }
 
-inputKey = c(0,0,0,1,0,1,1,0,1,0,1,1,1,1,0,1,0,1,0,0,0,0,0,0,1,0,0,1,0,1,0,0,1,0,1,1,0,1,0,0,0,1,0,1,0,0,1,1,1,0,0,0,1,0,1,0,0,1,1,1,1,0,1,1)
-permute(inputKey, TRUE)
+xInput = c(0,0,0,1,0,1,1,0,1,0,1,1,1,1,0,1,0,1,0,0,0,0,0,0,1,0,0,1,0,1,0,0,1,0,1,1,0,1,0,0,0,1,0,1,0,0,1,1,1,0,0,0,1,0,1,0,0,1,1,1,1,0,1,1)
+xPerm = permute(xInput, TRUE)
+
+permuteChoice1 = function(data) {
+    dataTemp = c(1:56)
+
+    for(i in 1:56) {
+        dataTemp[i] = data[pc1[i]]           
+    }
+
+    return(dataTemp)
+}
+
+keyInput = c(1,1,1,0,0,0,0,1,1,0,1,0,0,1,0,0,1,0,1,1,1,0,1,1,1,0,0,1,1,0,1,1,1,1,0,0,0,0,1,0,1,1,1,0,0,0,1,1,0,0,1,0,0,1,0,0,1,0,1,1,1,0,1,1)
+keyPerm = permuteChoice1(keyInput)
+
+chunk = function(x,n) split(x, cut(seq_along(x), n, labels = FALSE)) 
+
+keyPermL = chunk(keyPerm, 2)[[1]]
+keyPermR = chunk(keyPerm, 2)[[2]]
+
+shifter = function(data, round) {
+    rows = 4
+    cols = 7
+
+    tempMatrix = matrix(data, rows, cols, byrow = TRUE)
+    col1 = c(tempMatrix[1], tempMatrix[2], tempMatrix[3], tempMatrix[4])
+
+    posCol1 = c(1:4)
+    for (i in 1:shifts[round]) {
+        for (j in 1:4) {
+            if (posCol1[j] + 1 > 4)
+                posCol1[j] = 1
+            else 
+                posCol1[j] = posCol1[j] + 1
+        }
+    }
+
+    col1 = col1[c(posCol1)]
+
+    tempMatrix = cbind(tempMatrix, col1)
+    newMatrix = matrix(,rows,cols)
+    start = (rows * round) + 1
+    count = 1
+    for (i in start:length(tempMatrix)) {
+        newMatrix[count] = tempMatrix[i]
+
+        count = count + 1         
+    }
+
+    return(as.vector(t(newMatrix)))
+}
+
+keyPermLnew = shifter(keyPermL, 1)
+keyPermRnew = shifter(keyPermR, 1)
+
+keyPermNew = c(keyPermLnew, keyPermRnew)
+
+permuteChoice2 = function(data) {
+    dataTemp = c(1:48)
+
+    for(i in 1:48) {
+        dataTemp[i] = data[pc2[i]]           
+    }
+
+    return(dataTemp)
+}
+
+k1 = permuteChoice2(keyPermNew)
+
+xPermL = chunk(xPerm, 2)[[1]]
+xPermR = chunk(xPerm, 2)[[2]]
+
+permuteDbox = function(data) {
+    dataTemp = c(1:48)
+
+    for(i in 1:48) {
+        dataTemp[i] = data[dBox[i]]           
+    }
+
+    return(dataTemp)
+}
+
+eR0 = permuteDbox(xPermR)
+
+xorB = function(data1, data2) {
+    if (length(data1) == length(data2)) {
+        dataXOR = c(1:length(data1))
+
+        for(i in 1:length(data1)) {
+            if (xor(data1[i],data2[i])) {
+                dataXOR[i] = 1
+            } else {
+                dataXOR[i] = 0
+            }
+        }
+
+        return(dataXOR)
+    }
+}
+
+B = xorB(eR0, k1)
+
